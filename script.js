e3655fd7ccb26f8d90fc9745533ba891a3f40119
TanyaDanilovich <tan.danilovich@gmail.com>
2022-08-17 21:55:36 +0300

    resolve the kata santaclausable-interface

---
+++ script.js
@@ -0,0 +1,4 @@
+//www.codewars.com/kata/santaclausable-interface
+https: function isSantaClausable(obj) {
+  return 'functon' === typeof (obj.distributeGifts && obj.goDownTheChimney && obj.sayHoHoHo);
+}
