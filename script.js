df2588820e30675c6d185d39f7dfe291a00e5675
TanyaDanilovich <tan.danilovich@gmail.com>
2022-08-18 12:00:58 +0300

    resolve the kata train javascript

--- script.js
+++ script.js
@@ -2,3 +2,14 @@
 https: function isSantaClausable(obj) {
   return 'functon' === typeof (obj.distributeGifts && obj.goDownTheChimney && obj.sayHoHoHo);
 }
+
+//https://www.codewars.com/kata/513e1e47c600c93cef000001/train/javascript
+class Animal {
+  constructor(name, animal) {
+    this.name = name;
+    this.type = animal;
+  }
+  toString() {
+    return `${this.name} is a ${this.type}`;
+  }
+}
